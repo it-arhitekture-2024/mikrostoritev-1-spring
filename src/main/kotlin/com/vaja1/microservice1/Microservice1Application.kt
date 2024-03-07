d0845a798352be0ce135ac4e026bf936f4b205e0
89acat <tamara.jevtic@student.um.si>
2024-03-07 21:42:53 +0100

    Test Run

--- src/main/kotlin/com/vaja1/microservice1/Microservice1Application.kt
+++ src/main/kotlin/com/vaja1/microservice1/Microservice1Application.kt
@@ -2,8 +2,6 @@ package com.vaja1.microservice1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
